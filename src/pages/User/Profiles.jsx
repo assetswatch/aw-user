293038687a0f2633ea16c7a05005b9b2bba05b36
chandra sekhar <ncs.assetswatch@gmail.com>
2024-12-24 23:11:49 +0530

    Profiles UI Pages

--- src/pages/User/Profiles.jsx
+++ src/pages/User/Profiles.jsx
@@ -124,6 +124,74 @@ const Profiles = () => {
           </div>
         </div>
       </div>
+      <div className="full-row  bg-light">
+        <div className="container">
+          <div className="row mx-auto col-lg-8 shadow">
+            <div className="bg-white xs-p-20 p-30 pb-30 border rounded">
+              <h4 className="mb-4 down-line">Profiles</h4>
+              <div className="row row-cols-lg-3 pt-10 pb-0 row-cols-1 g-4 flex-center">
+                <div className="col">
+                  <div className="text-center px-4 py-10">
+                    <div>
+                      <Link to={routeNames.addownerprofile.path}>
+                        <i class="fa fa-plus-circle fa-3x"></i>
+                      </Link>
+                    </div>
+                    <div className="mt-3">
+                      <span className="transation font-500">
+                        <Link to={routeNames.addownerprofile.path}>Owner</Link>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="text-center px-4 py-10">
+                    <div>
+                      <Link to={routeNames.addagentprofile.path}>
+                        <i class="fa fa-plus-circle fa-3x"></i>
+                      </Link>
+                    </div>
+                    <div className="mt-3">
+                      <span className="transation font-500">
+                        <Link to={routeNames.addagentprofile.path}>Agent</Link>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="text-center px-4 py-10">
+                    <div>
+                      <Link to={routeNames.addtenantprofile.path}>
+                        <i class="fa fa-plus-circle fa-3x"></i>
+                      </Link>
+                    </div>
+                    <div className="mt-3">
+                      <span className="transation font-500">
+                        <Link to={routeNames.addtenantprofile.path}>
+                          Tenant
+                        </Link>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <hr className="w-100 text-primary d -none"></hr>
+                <div className="w-100 pt-10 pr-10 d-flex flex-end mt-0">
+                  <Link
+                    className="font-500 font-general text-primary"
+                    to={routeNames.dashboard.path}
+                  >
+                    <u>
+                      Skip
+                      <i className="pl-5 fa fa-circle-chevron-right"></i>
+                    </u>
+                    {/* <u>Cancel</u> */}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
